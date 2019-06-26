5fb5a050d6f968b8aca76817ae642eafd8a04e82
ViljaKettu <31925883+ViljaKettu@users.noreply.github.com>
2019-06-26 18:30:04 +0300

    colors[] test

--- exercise1.js
+++ exercise1.js
@@ -11,23 +11,29 @@ window.onload = function()
         context.beginPath();
         var x=Math.random()*width;
         var y=Math.random()*height;
-        var size = Math.random()*(maxSize-minSize) +minSize;
-        //size *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+        var size = Math.floor(Math.random()*(maxSize-minSize) +minSize);
+        size *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
 
         context.moveTo(x, y);           
         context.lineTo(x+Math.random()*size, y+Math.random()*size);
         context.lineTo(x+Math.random()*size, y+Math.random()*size);
 
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var j = 0; j < 6; j++) 
+        //var letters = '0123456789ABCDEF';
+        //var color = '#';
+        //for (var j = 0; j < 6; j++) 
+        //{
+        //color += letters[Math.floor(Math.random() * 16)];
+        //}
+
+        var colors =[];
+        for(var j = 0; j< 3; j++)
         {
-        color += letters[Math.floor(Math.random() * 16)];
+            colors.push(Math.floor(x*255));
         }
 
-        console.log(color);
+        console.log(colors);
         
-        context.fillStyle = color;
+        context.fillStyle = colors;
         context.fill();
     }
    
