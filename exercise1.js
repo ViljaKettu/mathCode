4fb7c59d9dd73fbd51fd3bf8f4124125ed88ec74
ViljaKettu <31925883+ViljaKettu@users.noreply.github.com>
2019-06-26 14:23:05 +0300

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -1,15 +1,37 @@
 window.onload = function()
 {
+
+
     var canvas = document.getElementById("canvas"),
         context = canvas.getContext("2d"),
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight;
 
-    for(var i = 0; i<100; i++)
+    for(var i= 0; i<10; i++)
     {
+        
         context.beginPath();
-        context.moveTo(Math.random()*width, Math.random()*height);
-        context.lineTo(Math.random()*width, Math.random() *height);
-        context.stroke();
+        for(var j = 0; j<5; j++)
+        {
+            context.moveTo(Math.random()*width, Math.random()*height);
+            context.lineTo(Math.random()*width, Math.random() *height);
+            context.stroke();
+    
+        }
+        context.closePath();
+
+        context.fillstyle = getRandomColor();
+        context.fill();
     }
+   
 };
+
+function getRandomColor()
+{
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
