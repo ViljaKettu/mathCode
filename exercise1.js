3aad591cfb509fbc19cc9f9474f314f1c4ec9a27
ViljaKettu <31925883+ViljaKettu@users.noreply.github.com>
2019-06-26 17:28:44 +0300

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -9,8 +9,8 @@ window.onload = function()
     {
         context.beginPath();
         context.moveTo(Math.random()*width, Math.random()*height);           
-        context.lineTo(Math.random()*width/4, Math.random() *height/4);
-        context.lineTo(Math.random()*width/4, Math.random() *height/4);
+        context.lineTo(Math.random()*width, Math.random() *height);
+        context.lineTo(Math.random()*width, Math.random() *height);
 
         var letters = '0123456789ABCDEF';
         var color = '#';
@@ -24,5 +24,26 @@ window.onload = function()
         context.fillStyle = color;
         context.fill();
     }
-   
+
+    for(var i= 0; i<30; i++)
+    {
+        context.beginPath();
+        context.moveTo(Math.random()*width, Math.random()*height);           
+        context.lineTo(Math.random()*width, Math.random() *height);
+        context.lineTo(Math.random()*width, Math.random() *height);
+        context.lineTo(Math.random()*width, Math.random() *height);
+
+
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var j = 0; j < 6; j++) 
+        {
+        color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        console.log(color);
+        
+        context.fillStyle = color;
+        context.fill();
+    }
 };
