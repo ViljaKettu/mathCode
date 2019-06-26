f831d2de8368a376de3ff9d97f8308f3ebbaaebe
ViljaKettu <31925883+ViljaKettu@users.noreply.github.com>
2019-06-26 14:48:09 +0300

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -10,18 +10,26 @@ window.onload = function()
 
     for(var i= 0; i<10; i++)
     {
-        
         let region = new Path2D();
-        region.moveTo(30, 90);
-        region.lineTo(110, 20);
-        region.lineTo(240, 130);
-        region.lineTo(60, 130);
-        region.lineTo(190, 20);
-        region.lineTo(270, 90);
-        region.closePath();
+        region.beginPath();
+        region.moveTo(Math.random()*width, Math.random()*height);
+        for(var j = 0; j<5; j++)
+        {
+            
+            region.lineTo(Math.random()*width, Math.random() *height);
+            
+            //region.stroke();
+    
+        }
+        
 
-        context.fillstyle = 'green';
-        context.fill(region, 'evenodd');
+        
+        for (var k = 0; k < 6; k++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        context.fillstyle = color;
+        context.fill();
     }
    
 };
